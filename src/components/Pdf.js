c38ed30e60ec73bc3cb52eb8a502b75ae82650e1
stormzy22 <aawweb23@gmail.com>
2021-07-07 14:26:47 +0100

    added upload and view functionality

--- src/components/Pdf.js
+++ src/components/Pdf.js
@@ -1,121 +1,146 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import styled from "styled-components";
+
 const url = "https://arxiv.org/pdf/quant-ph/0410100.pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-export default function Test() {
-  // https://arxiv.org/pdf/quant-ph/0410100.pdf
-
+const Test = () => {
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-  const [zoom, setZoom] = useState(2);
+  const [zoom, setZoom] = useState(1);
+  const [localUrl, setLocalUrl] = useState(null);
 
   /*To Prevent right click on screen*/
   document.addEventListener("contextmenu", (event) => {
     event.preventDefault();
   });
-
   /*When document gets loaded successfully*/
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
     setPageNumber(1);
+    setZoom(1);
   }
-
   function changePage(offset) {
     setPageNumber((prevPageNumber) => prevPageNumber + offset);
   }
-
   function previousPage() {
     changePage(-1);
   }
-
   function nextPage() {
     changePage(1);
   }
 
-  return (
-    <>
-      <Main className="main">
-        <div className="d-flex mb-2 py-1 justify-content-end toggle-zoom">
-          <div className="d-flex toggle-zoom-main">
-            <button
-              className="btn toggle-zoom-minus"
-              onClick={() => setZoom((c) => c - 1)}
-              disabled={zoom <= 2}
-            >
-              <i className="fas fa-search-minus"></i>
-            </button>
-            <button
-              className="toggle-zoom-plus btn"
-              onClick={() => setZoom((c) => c + 1)}
-              disabled={zoom >= 5}
-            >
-              <i className="fas fa-search-plus"></i>
-            </button>
-          </div>
-        </div>
-        <Document
-          file={url}
-          className="pdfViewer"
-          onLoadSuccess={onDocumentLoadSuccess}
-          id="text"
-        >
-          <Page pageNumber={pageNumber} scale={zoom} className="pdfViewer_" />
-        </Document>
-        <div className=" mt-3 py-4 border-darken-4 text-dark control-nav">
-          {/* <div className="pdf-progress py-1 border border-5 rounded-pill mb-3">
-            <div
-              className="dot rounded-circle"
-              style={{
-                left: `${Math.round((pageNumber / numPages) * 100) - 2}%`,
-              }}
-            ></div>
-          </div> */}
-          <div className="progress">
-            <div
-              className="progress-bar progress-bar-striped"
-              role="progressbar"
-              style={{ width: `${Math.round((pageNumber / numPages) * 100)}%` }}
-              aria-valuenow="25"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-          <p className="text-end mb-2 fw-bold fs-5">
-            {Math.round((pageNumber / numPages) * 100)}% read
-          </p>
-          <div className="d-flex control-nav-main justify-content-center align-items-center">
-            <button
-              type="button"
-              disabled={pageNumber <= 1}
-              onClick={previousPage}
-              className="Pre btn p-0 bg-transparent"
-            >
-              <i className="fas fa-2x fa-caret-left"></i>
-            </button>
-            <div className="pagec">
-              Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
-            </div>
-            <button
-              type="button"
-              disabled={pageNumber >= numPages}
-              onClick={nextPage}
-              className="btn p-0 bg-transparent"
-            >
-              <i className="fas fa-2x fa-caret-right"></i>
-            </button>
-          </div>
-        </div>
-      </Main>
-    </>
-  );
-}
+  const getPdf = (e) => {
+    const doc = e.target.files[0];
+    const data = [];
+    data.push(doc);
+    const urlDoc = window.URL.createObjectURL(
+      new Blob(data, { type: "application/pdf" })
+    );
+    setLocalUrl(urlDoc);
+  };
 
+  return (
+    <main className="bg-light">
+      <section className="py-2 flex-grow-1">
+        <Main className="main container">
+          <form className="mb-2">
+            <input
+              type="file"
+              className="form-control"
+              onChange={(e) => getPdf(e)}
+              placeholder="upload PDF..."
+            />
+          </form>
+          <div className="d-flex mb-2 py-1 justify-content-end toggle-zoom">
+            <div className="d-flex toggle-zoom-main">
+              <button
+                className="btn toggle-zoom-minus"
+                onClick={() => setZoom((c) => c - 1)}
+                disabled={zoom <= 1}
+                aria-label="zoom out"
+              >
+                <i className="fas fa-search-minus"></i>
+              </button>
+              <button
+                className="toggle-zoom-plus btn"
+                onClick={() => setZoom((c) => c + 1)}
+                disabled={zoom >= 5}
+                aria-label="zoom in"
+              >
+                <i className="fas fa-search-plus"></i>
+              </button>
+            </div>
+          </div>
+          <Document
+            file={localUrl ? localUrl : url}
+            className="pdfViewer"
+            onLoadSuccess={onDocumentLoadSuccess}
+            id="text"
+            renderMode="canvas"
+            onLoadError={() => {
+              setLocalUrl(null);
+              alert(`Can't load pdf`);
+            }}
+            error={`cant't load PDF`}
+            loading="loading........."
+          >
+            <Page pageNumber={pageNumber} scale={zoom} className="pdfViewer_" />
+          </Document>
+          <div className=" mt-1 py-1 border-darken-4 text-dark control-nav">
+            <div className="progress">
+              <div
+                className="progress-bar progress-bar-striped"
+                role="progressbar"
+                style={{
+                  width: `${Math.round((pageNumber / numPages) * 100)}%`,
+                }}
+                aria-label="progress"
+                aria-valuenow={pageNumber}
+                aria-valuemin={0}
+                aria-valuemax={100}
+              ></div>
+            </div>
+            <p className="text-end mb-1 fw-bold fs-5">
+              {Math.round((pageNumber / numPages) * 100)}% read
+            </p>
+            <div className="d-flex control-nav-main justify-content-center align-items-center">
+              <button
+                type="button"
+                disabled={pageNumber <= 1}
+                onClick={previousPage}
+                className="Pre btn p-0 bg-transparent"
+                aria-label="arrow left"
+              >
+                <i className="fas fa-2x fa-caret-left"></i>
+              </button>
+              <div className="pagec">
+                Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+              </div>
+              <button
+                type="button"
+                disabled={pageNumber >= numPages}
+                onClick={nextPage}
+                className="btn p-0 bg-transparent"
+                aria-label="arrow right"
+              >
+                <i className="fas fa-2x fa-caret-right"></i>
+              </button>
+            </div>
+          </div>
+        </Main>
+      </section>
+    </main>
+  );
+};
+
+export default Test;
 const Main = styled.div`
   user-select: none;
-  width: 100%;
-  min-height: 100vh;
+  width: 90%;
+  max-width: 100000000px;
+  min-height: 95vh;
   .toggle-zoom {
     &-main {
       gap: 2rem;
@@ -125,9 +150,10 @@ const Main = styled.div`
     }
   }
   .pdfViewer {
-    min-height: inherit !important;
+    width: 100% !important;
+    margin: 0 auto;
     &_ {
-      height: 1600px;
+      /* min-height: 90vh; */
       overflow: auto;
       section {
         display: none !important;
@@ -135,6 +161,8 @@ const Main = styled.div`
       canvas {
         direction: none;
         margin: 0 auto;
+        /* height: auto !important; */
+        width: inherit !important;
       }
     }
   }
